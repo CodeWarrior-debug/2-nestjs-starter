9059ee9be70c66668deb24cef7f18717dbbe4caa
James Jordan <jpjordan1987@gmail.com>
2023-11-27 18:40:59 -0500

    test logging fix

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,9 +1,15 @@
-import { Global, Module } from '@nestjs/common';
+import {
+  Global,
+  MiddlewareConsumer,
+  Module,
+  RequestMethod,
+} from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import config from 'src/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TransformResponseInterceptor } from './interceptors/transform-respons/transform-respons.interceptor';
 import { LoggerService } from './logger/logger.service';
+import { LoggerMiddleware } from './logger/logger.middleware';
 @Global()
 @Module({
   imports: [
@@ -21,4 +27,11 @@ import { LoggerService } from './logger/logger.service';
   ],
   exports: [LoggerService],
 })
-export class CoreModule {}
+export class CoreModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
+  }
+}
